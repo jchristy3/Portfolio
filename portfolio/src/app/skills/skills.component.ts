349353efb808ad3183664f87d258e9bf7fd9e726
John Christy <christyjohn14@gmail.com>
2023-08-21 11:37:15 -0400

    added more skills

--- portfolio/src/app/skills/skills.component.ts
+++ portfolio/src/app/skills/skills.component.ts
@@ -32,6 +32,13 @@ export class SkillsComponent implements OnInit {
     {id: 3, name: 'Visual Studio Code', experienceLevel: 4, yearsExperience: 5, categories: ['frontend', 'ide'], tags: ['angular', 'visual', 'studio', 'code', 'vs', 'html', 'css']},
     {id: 4, name: 'Visual Studio', experienceLevel: 5, yearsExperience: 8, categories: ['ide', 'backend'], tags: ['ssrs', 'c#', 'c++', 'asp.net', '.net', 'unit', 'testing', 'api', 'moq', '2017', '2019', '2022']},
     {id: 5, name: 'ASP.NET', experienceLevel: 4, yearsExperience: 4, categories: ['data', 'framework', 'backend'], tags: ['asp', '.net', 'asp.net', 'c#', 'sql', 'code-first', 'database-first']},
+    {id: 6, name: 'C#', experienceLevel: 4, yearsExperience: 4, categories: ['data', 'language', 'backend'], tags: ['asp', '.net', 'asp.net', 'c#', 'visual-studio']},
+    {id: 7, name: 'Typescript', experienceLevel: 4, yearsExperience: 4, categories: ['frontend', 'language'], tags: ['angular', 'visual', 'studio', 'code', 'visual-studio-code', 'typescript']},
+    {id: 7, name: 'Angular', experienceLevel: 4, yearsExperience: 4, categories: ['frontend', 'framework'], tags: ['angular', 'visual', 'studio', 'code', 'visual-studio-code', 'typescript']},
+    {id: 7, name: 'HTML', experienceLevel: 4, yearsExperience: 5, categories: ['frontend', 'language'], tags: ['html', 'visual', 'studio', 'code', 'visual-studio-code']},
+    {id: 8, name: 'CSS', experienceLevel: 4, yearsExperience: 5, categories: ['frontend', 'language'], tags: ['css', 'visual', 'studio', 'code', 'visual-studio-code']},
+    {id: 9, name: 'Bootstrap', experienceLevel: 2, yearsExperience: 2, categories: ['frontend', 'tools'], tags: ['bootstrap', 'visual', 'studio', 'code', 'visual-studio-code', 'css', 'html', 'angular']},
+    {id: 10, name: '.NET Framework', experienceLevel: 4, yearsExperience: 4, categories: ['backend', 'framework'], tags: ['.net', 'asp', '.net', 'asp.net', 'c#', 'sql', 'code-first', 'database-first']},
   ];
   skillsShown: any[] = [];
   tags = ['ssrs', 'sql', 'server', 'reporting', 'services', 'excel', 'microsoft', 'word','ssms', 'server', 'management', 'studio'];
