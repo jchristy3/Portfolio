6a6515190b1c14c4371d734f6be40fe26535dede
John Christy <christyjohn14@gmail.com>
2023-08-25 16:16:15 -0400

    more updates to project
    replace mat-cards with divs to remove spacing at bottom of page

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
-  projects: any = [{name: 'Portfolio', icon: 'fa-solid fa-code fa-2xl', tooltip: 'The repository for this portfolio page', link: 'https://github.com/jchristy3/Portfolio'},
-    {name: 'FPS_Playground', icon: 'fa-brands fa-unity fa-2xl', tooltip: 'Unity test project for learning 3D game development', link: 'https://github.com/jchristy3/FPS_Playground'}                
+  projects: any = [{name: 'Portfolio', icon: 'fa-brands fa-angular fa-2xl', tooltip: 'The repository for this portfolio page', link: 'https://github.com/jchristy3/Portfolio'},
+    {name: 'FPS_Playground', icon: 'fa-brands fa-unity fa-2xl', tooltip: 'Unity test project for learning 3D game development', link: 'https://github.com/jchristy3/FPS_Playground'},
+    {name: 'cs-recruit-retain', icon: 'fa-solid fa-code fa-2xl', tooltip: 'College Ruby on Rails Project', link: 'https://github.com/CitadelCS/cs-recruit-retain'}
   ]
 
   goToLink(url: string) {
