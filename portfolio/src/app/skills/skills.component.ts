23c859518c4fdf5f7c7deeaa378e23d3fa46dd78
John Christy <christyjohn14@gmail.com>
2023-09-28 18:12:53 -0400

    rounded out skills and wrote intro blurb

--- portfolio/src/app/skills/skills.component.ts
+++ portfolio/src/app/skills/skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { Skill } from 'src/models/skill';
+import { SKILLS, TAGS, CATEGORIES } from 'src/constants/skills-constants';
 
 @Component({
   selector: 'app-skills',
@@ -16,36 +17,12 @@ export class SkillsComponent implements OnInit {
   showAdvancedFilters: boolean = false;
   starRating = 0;
   yearsExperience: string[] = ['<1', '1', '2', '3', '4', '5+'];
-  skillCategories = [
-    {value: 'frontend', viewValue: 'Front-End Development'},
-    {value: 'backend', viewValue: 'Back-End Development'},
-    {value: 'data', viewValue: 'Data and Reporting'},
-    {value: 'ide', viewValue: 'IDEs'},
-    {value: 'language', viewValue: 'Languages'},
-    {value: 'framework', viewValue: 'Frameworks'},
-    {value: 'tools', viewValue: 'Tools and Components'},
-    {value: 'principle', viewValue: 'Practices and Principles'}
-  ];
+  skillCategories = CATEGORIES;
   orderBySelection = 'experience';
   orderDirectionSelection = 'DESC';
-  currentDate: Date = new Date();
-  //currentDate: string = formatDate(new Date(), 'yyyy-MM-dd', 'en-US');
-  skills: Skill[] = [
-    {id: 1, name: 'SSRS', experienceLevel: 4, yearsExperience: 3, lastUsed: this.currentDate, description: 'test description', categories: ['data', 'tools'], tags: ['ssrs', 'sql', 'server', 'reporting', 'services', 'excel', 'microsoft', 'word']},
-    {id: 2, name: 'SSMS', experienceLevel: 4, yearsExperience: 5, lastUsed: this.currentDate, description: 'test description', categories: ['data', 'tools', 'ide'], tags: ['ssms', 'sql', 'server', 'management', 'studio']},
-    {id: 3, name: 'Visual Studio Code', experienceLevel: 4, yearsExperience: 5, lastUsed: this.currentDate, description: 'test description', categories: ['frontend', 'ide'], tags: ['angular', 'visual', 'studio', 'code', 'vs', 'html', 'css']},
-    {id: 4, name: 'Visual Studio', experienceLevel: 5, yearsExperience: 8, lastUsed: this.currentDate, description: 'test description', categories: ['ide', 'backend'], tags: ['ssrs', 'c#', 'c++', 'asp.net', '.net', 'unit', 'testing', 'api', 'moq', '2017', '2019', '2022']},
-    {id: 5, name: 'ASP.NET', experienceLevel: 4, yearsExperience: 4, lastUsed: this.currentDate, description: 'test description', categories: ['data', 'framework', 'backend'], tags: ['asp', '.net', 'asp.net', 'c#', 'sql', 'code-first', 'database-first']},
-    {id: 6, name: 'C#', experienceLevel: 4, yearsExperience: 4, lastUsed: this.currentDate, description: 'test description', categories: ['data', 'language', 'backend'], tags: ['asp', '.net', 'asp.net', 'c#', 'visual-studio']},
-    {id: 7, name: 'Typescript', experienceLevel: 4, yearsExperience: 4, lastUsed: this.currentDate, description: 'test description', categories: ['frontend', 'language'], tags: ['angular', 'visual', 'studio', 'code', 'visual-studio-code', 'typescript']},
-    {id: 7, name: 'Angular', experienceLevel: 4, yearsExperience: 4, lastUsed: this.currentDate, description: 'test description', categories: ['frontend', 'framework'], tags: ['angular', 'visual', 'studio', 'code', 'visual-studio-code', 'typescript']},
-    {id: 7, name: 'HTML', experienceLevel: 4, yearsExperience: 5, lastUsed: this.currentDate, description: 'test description', categories: ['frontend', 'language'], tags: ['html', 'visual', 'studio', 'code', 'visual-studio-code']},
-    {id: 8, name: 'CSS', experienceLevel: 4, yearsExperience: 5, lastUsed: this.currentDate, description: 'test description', categories: ['frontend', 'language'], tags: ['css', 'visual', 'studio', 'code', 'visual-studio-code']},
-    {id: 9, name: 'Bootstrap', experienceLevel: 2, yearsExperience: 2, lastUsed: this.currentDate, description: 'test description', categories: ['frontend', 'tools'], tags: ['bootstrap', 'visual', 'studio', 'code', 'visual-studio-code', 'css', 'html', 'angular']},
-    {id: 10, name: '.NET Framework', experienceLevel: 4, yearsExperience: 4, lastUsed: this.currentDate, description: 'test description', categories: ['backend', 'framework'], tags: ['.net', 'asp', '.net', 'asp.net', 'c#', 'sql', 'code-first', 'database-first']},
-  ];
+  skills: Skill[] = SKILLS;
   skillsShown: any[] = [];
-  tags = ['ssrs', 'sql', 'server', 'reporting', 'services', 'excel', 'microsoft', 'word','ssms', 'server', 'management', 'studio'];
+  tags = TAGS;
   skillSelected: any;
   tagsSelected: string[] = [];
 
